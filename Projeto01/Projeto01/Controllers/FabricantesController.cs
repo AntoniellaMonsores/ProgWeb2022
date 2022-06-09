0920b981f7b2cb0bd8979ca41c3524ef41d2ba69
Antoniella Monsores <antoniella.monsores@gmail.com>
2022-06-09 10:54:43 -0300

    Atualização

--- Projeto01/Projeto01/Controllers/FabricantesController.cs
+++ Projeto01/Projeto01/Controllers/FabricantesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Projeto01.Models;
+using Projeto01.Context;
 
 namespace Projeto01.Controllers
 {
