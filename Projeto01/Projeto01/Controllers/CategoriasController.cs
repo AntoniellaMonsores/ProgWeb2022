4ffc0fda8c52b3319ad0b386cf584e264bc61986
Antoniella Monsores <antoniella.monsores@gmail.com>
2022-07-18 10:14:32 -0300

    Update

--- Projeto01/Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Projeto01/Controllers/CategoriasController.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Projeto01.Context;
 using Projeto01.Models;
 
 namespace Projeto01.Controllers
 {
     public class CategoriasController : Controller
     {
+        /*
         private static IList<Categoria> categorias = new List<Categoria>()
         {
             new Categoria() { CategoriaId = 1, Nome = "Notebooks"},
@@ -17,33 +21,53 @@ namespace Projeto01.Controllers
             new Categoria() { CategoriaId = 4, Nome = "Mouses"},
             new Categoria() { CategoriaId = 5, Nome = "Desktops"}
         };
+        */
 
+        private EFContext context = new EFContext();
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(context.Categorias.OrderBy(c => c.Nome));
+        }
+
+        /*
         // GET: Categorias
         public ActionResult Index()
         {
             return View(categorias);
         }
-        
-        // GET: Categorias
+        */
+ 
+        // GET: Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Categorias Create
+        // POST: Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Categoria categoria)
         {
-            categorias.Add(categoria);
-            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            context.Categorias.Add(categoria);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        // GET: Categorias Edit
+        /*// POST: Categorias Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            context.Categorias.Add(categoria);
+            categoria.CategoriaId = context.Categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }*/
+
+        /*// GET: Categorias Edit
         public ActionResult Edit(long id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            return View(context.Categorias.Where(m => m.CategoriaId == id).First());
         }
 
         // POST: Categorias Edit
@@ -51,21 +75,50 @@ namespace Projeto01.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Categoria categoria)
         {
-            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
-            categorias.Add(categoria);
+            context.Categorias.Remove(context.Categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            context.Categorias.Add(categoria);
             return RedirectToAction("Index");
+        }*/
+
+        // GET: Edit
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
         }
 
-        // GET: Categorias Details
+        // POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        /*// GET: Categorias Details
         public ActionResult Details(long id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            return View(context.Categorias.Where(m => m.CategoriaId == id).First());
         }
 
         // GET: Categorias Delete
         public ActionResult Delete(long id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            return View(context.Categorias.Where(m => m.CategoriaId == id).First());
         }
 
         // POST: Categorias Delete
@@ -73,7 +126,49 @@ namespace Projeto01.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Delete(Categoria categoria)
         {
-            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            context.Categorias.Remove(context.Categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
+        }*/
+
+        // GET: Details
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categorias = context.Categorias.Find(id);
+            if (categorias == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categorias);
+        }
+
+        // GET: Delete
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Categoria categoria = context.Categorias.Find(id);
+            context.Categorias.Remove(categoria);
+            context.SaveChanges();
+            TempData["Message"] = "Categoria " + categoria.Nome.ToUpper() + " foi removida";
             return RedirectToAction("Index");
         }
     }
