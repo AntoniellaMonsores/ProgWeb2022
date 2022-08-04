68bda6d449fa5c05fb2b4dd812e924d0f3e033f9
Antoniella Monsores <antoniella.monsores@academico.ifrn.edu.br>
2022-08-04 10:53:43 -0300

    Update

---
+++ Projeto01/Projeto01/Controllers/ProdutosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01.Context;
+
+namespace Projeto01.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View(context.Produtos.OrderBy(c => c.Nome));
+        }
+    }
+}
